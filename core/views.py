247021f50196fe3e888cfb29c87412dbe9474aa6
Berk Alp Yakici <berkalp.y@gmail.com>
2019-07-12 13:54:04 -0500

    Fix FE display

--- core/views.py
+++ core/views.py
@@ -32,19 +32,30 @@ class FrontendAppView(View):
     Serves the compiled frontend entry point (only works if you have run `yarn
     build`).
     """
-    index_file_path = os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')
 
     def get(self, request):
+        build_path = os.path.join(settings.REACT_APP_DIR, 'build')
+
+        if request.path_info != '/':
+            file_path = build_path+request.path_info
+        else:
+            file_path = os.path.join(build_path, 'index.html')
+
         try:
-            with open(self.index_file_path) as f:
+            with open(file_path) as f:
                 return HttpResponse(f.read())
         except FileNotFoundError:
-            logging.exception('Production build of app not found')
-            return HttpResponse(
-                """
-                This URL is only used when you have built the production
-                version of the app. Visit http://localhost:3000/ instead after
-                running `yarn start` on the frontend/ directory
-                """,
-                status=501,
-            )
+            if request.path_info == '/':
+                logging.exception('Production build of app not found')
+                return HttpResponse(
+                    """
+                    This URL is only used when you have built the production
+                    version of the app. Visit http://localhost:3000/ instead after
+                    running `yarn start` on the webview/ directory
+                    """,
+                    status=501,
+                )
+            else:
+                return HttpResponse(
+                    status=404,
+                )
