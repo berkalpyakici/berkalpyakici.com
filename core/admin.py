b6c94777d50d1597a493c3bd2d9c0bb2d090c3d7
Berk Alp Yakici <berkalp.y@gmail.com>
2021-02-03 11:42:26 -0600

    Clear

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Setting, Social, Project, Experience
-
-admin.site.register(Setting)
-admin.site.register(Social)
-admin.site.register(Project)
-admin.site.register(Experience)
